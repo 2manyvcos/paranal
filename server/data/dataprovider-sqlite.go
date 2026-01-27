20dc2ac50f570295970d97419f33013abe1cb43a
Aaron <4709780+2manyvcos@users.noreply.github.com>
2026-01-27 19:12:40 +0000

    DB migration notes

--- server/data/dataprovider-sqlite.go
+++ server/data/dataprovider-sqlite.go
@@ -32,6 +32,20 @@ func SQLite(path string) (DataProvider, error) {
 func (p *sqlite) Setup() error {
 	log.Println("Setting up SQLite data backend")
 
+	// Migration strategy as taken from a stackoverflow comment:
+
+	// > For tracking the database version, I use the built in user-version variable that sqlite provides (sqlite does nothing with this variable, you are free to use it however you please). It starts at 0, and you can get/set this variable with the following sqlite statements:
+	// >
+	// > > PRAGMA user_version;
+	// > > PRAGMA user_version = 1;
+	// >
+	// > When the app starts, I check the current user-version, apply any changes that are needed to bring the schema up to date, and then update the user-version. I wrap the updates in a transaction so that if anything goes wrong, the changes aren't committed.
+	// >
+	// > For making schema changes, sqlite supports "ALTER TABLE" syntax for certain operations (renaming the table or adding a column). This is an easy way to update existing tables in-place. See the documentation here: http://www.sqlite.org/lang_altertable.html. For deleting columns or other changes that aren't supported by the "ALTER TABLE" syntax, I create a new table, migrate data into it, drop the old table, and rename the new table to the original name.
+
+	// var dbVersion int
+	// err = p.DB.QueryRow("PRAGMA user_version").Scan(&dbVersion)
+
 	_, err := p.DB.Exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT UNIQUE, role INTEGER, pwHash TEXT)")
 	if err != nil {
 		return fmt.Errorf(`creating table "users" failed - %s`, err)
