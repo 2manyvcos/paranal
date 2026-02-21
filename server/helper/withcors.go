e75c082deb2c7ac8289f97a0495746945e5eace7
Aaron <4709780+2manyvcos@users.noreply.github.com>
2026-02-21 09:42:44 +0000

    Add preflight handling

--- server/helper/withcors.go
+++ server/helper/withcors.go
@@ -12,16 +12,44 @@ func WithCORS(handler http.Handler) http.Handler {
 		if app.Config.CORSOrigin != "" {
 			res.Header().Set("Access-Control-Allow-Origin", app.Config.CORSOrigin)
 		}
-		res.Header().Set("Access-Control-Allow-Credentials", "true")
-		allowedHeaders := []string{"Accept", "Content-Type", "Authorization"}
-		if app.Config.Auth.RemoteUser.Enabled {
-			allowedHeaders = append(allowedHeaders, app.Config.Auth.RemoteUser.HeaderName)
-			if app.Config.Auth.RemoteUser.GroupsHeaderName != "" {
-				allowedHeaders = append(allowedHeaders, app.Config.Auth.RemoteUser.GroupsHeaderName)
+
+		if req.Method == http.MethodOptions {
+			allowedHeaders := []string{"Accept", "Content-Type", "Authorization"}
+			if app.Config.Auth.RemoteUser.Enabled {
+				allowedHeaders = append(allowedHeaders, app.Config.Auth.RemoteUser.HeaderName)
+				if app.Config.Auth.RemoteUser.GroupsHeaderName != "" {
+					allowedHeaders = append(allowedHeaders, app.Config.Auth.RemoteUser.GroupsHeaderName)
+				}
 			}
+			res.Header().Set("Access-Control-Allow-Headers", strings.Join(allowedHeaders, ", "))
+
+			corsRes := corsResponseWriter{}
+			corsReq := req.Clone(req.Context())
+			handler.ServeHTTP(&corsRes, corsReq)
+			if allow := corsRes.header.Get("Allow"); allow != "" {
+				res.Header().Set("Access-Control-Allow-Methods", allow)
+			}
+
+			res.WriteHeader(http.StatusNoContent)
 		}
-		res.Header().Set("Access-Control-Allow-Headers", strings.Join(allowedHeaders, ", "))
 
 		handler.ServeHTTP(res, req)
 	})
 }
+
+type corsResponseWriter struct {
+	header http.Header
+}
+
+func (res *corsResponseWriter) Header() http.Header {
+	if res.header == nil {
+		res.header = make(http.Header)
+	}
+	return res.header
+}
+
+func (res *corsResponseWriter) Write([]byte) (int, error) {
+	return 0, nil
+}
+
+func (res *corsResponseWriter) WriteHeader(int) {}
